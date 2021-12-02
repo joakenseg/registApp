6c743cb7e34af05328731c30dd4d59598a5ec3f1
joakenseg <81238879+joakenseg@users.noreply.github.com>
2021-12-02 19:14:43 -0300

    creacion del la captura de qr

--- src/app/alumnos/alumnos.page.ts
+++ src/app/alumnos/alumnos.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Subscriber } from 'rxjs';
-import { ServiceAlumService } from '../servicios/service-alum.service'
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { LoadingController, ToastController } from '@ionic/angular';
+import jsQR from 'jsqr';
+
 
 @Component({
   selector: 'app-alumnos',
@@ -9,24 +10,118 @@ import { ServiceAlumService } from '../servicios/service-alum.service'
 })
 export class AlumnosPage implements OnInit {
 
-  listaAlum: any[]=[];
+  scanActive = false;
+  scanResult = null;
+  @ViewChild('video', { static: false}) video : ElementRef;
+  @ViewChild('canvas', { static: false}) canvas : ElementRef;
 
-  constructor(private servicio: ServiceAlumService) { 
-    this.traerDatos();
-  }
+  videoElement: any;
+  canvasElement: any;
+  canvasContext: any;
+
+  loading: HTMLIonLoadingElement = null;
+
+  constructor(private toastCtrl: ToastController, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
   }
 
-  traerDatos()
-  {
-    this.servicio.getAlumnos().subscribe((data)=>{
-      console.log(data)
-      this.listaAlum=data;
-  });
+  ngAfterViewInit(){
+    this.videoElement = this.video.nativeElement;
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasContext = this.canvasElement.getContext('2d');
   }
 
-  alerta(){
-    alert('aarg')
+  async startScan(){
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'enviroment'}
+    });
+    this.videoElement.srcObject = stream;
+    this.videoElement.setAttribute('playsinline', true);
+    this.videoElement.play();
+
+    this.loading = await this.loadingCtrl.create({});
+    await this.loading.present();
+
+    requestAnimationFrame(this.scan.bind(this));
   }
+
+  scan(){
+    console.log('SCAN');
+    
+    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA){
+      if (this.loading){
+        this.loading.dismiss();
+        this.loading = null;
+        this.scanActive = true;
+      }
+
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
+
+      this.canvasContext.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+
+      const code = jsQR(imageData.data, imageData.width, imageData.height,{
+        inversionAttempts: 'dontInvert'
+      });
+      console.log('code:', code);
+      
+      if (code){
+        console.log(code);
+        
+        this.scanActive = false;
+        this.scanResult = code.data;
+        this.showQrToast();
+      } else {
+        requestAnimationFrame(this.scan.bind(this));
+      }
+    }
+  }
+
+  // Helper functions
+  stopScan() {
+    this.scanActive = false;
+    const stream = this.videoElement.srcObject;
+    const tracks = stream.getTracks();
+    tracks.forEach(function(track) {
+      track.stop();
+    });
+    this.scanResult = null;
+    this.scan
+    
+    this.videoElement.srcObject = null;
+  }
+
+  reset(){
+    this.scanResult= null;
+  }
+
+  async showQrToast(){
+    const toast = await this.toastCtrl.create({
+      message: `Open ${this.scanResult}?`,
+      position: 'top',
+      buttons: [
+        {
+          text:'Open',
+          handler: () => {
+            window.open(this.scanResult, '_system', 'location=yes');
+          }
+        }
+      ]
+    });
+    toast.present();
+  } 
 }
